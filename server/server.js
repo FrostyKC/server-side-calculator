6c910ed492aff378b79df7fe7b7a17993f1f3862
Joe Thompson <smnindian30@yahoo.com>
2020-10-16 20:26:59 -0500

    addition works on the calculation part in the server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { log } = require('console');
 const app = express();
 const PORT = 5000;
 const history = [];
@@ -10,8 +11,33 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 
 // POST for receiving problems
+app.post('/problems', (req, res) => {
+  // problem data
+  // [
+  //   {
+  //     num1: '',
+  //     math: '',
+  //     num2: '',
+  //   }
+  // ]
+  const problemData = req.body;
+  console.log(problemData);
+  const results = []; // math problem
 
-// calculate problem
+  // calculate problem
+  for (let i = 0; i < problemData.length; i++) {
+    const problem = problemData[i];
+    if (problem.math === '+') {
+      problem.result = problem.num1 + problem.num2;
+    }
+    results.push(problem);
+  }
+
+  history.push(results);
+  console.log(history);
+
+  res.sendStatus(200);
+});
 
 // GET results/history
 app.get('/results', (req, res) => {
