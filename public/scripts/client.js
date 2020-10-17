1c7a3e43c094cd65864cf1ead892f64b8c79fc89
Joe Thompson <smnindian30@yahoo.com>
2020-10-16 22:15:46 -0500

    sending problems to the server

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,4 +2,38 @@ $(document).ready(onReady);
 
 function onReady() {
   console.log('ready');
+
+  $('.js-btn-sumt').on('click', clickHandlerSubmit);
+  $('.math').on('click', selectMath);
+}
+
+function clickHandlerSubmit() {
+  const problems = [
+    {
+      num1: $('.js-inpt-num1').val(),
+      math: $('.mathSelected').val(),
+      num2: $('.js-inpt-num2').val(),
+    },
+  ];
+}
+
+function selectMath() {
+  $(this).toggleClass('mathSelected');
+}
+
+// API / server calls
+
+function postProblems(problems) {
+  $.ajax({
+    type: 'POST',
+    url: '/problems',
+    data: problems,
+  })
+    .then(function (response) {
+      console.log('POST of problems:', response);
+    })
+    .catch(function (err) {
+      console.log(err);
+      alert('it broke');
+    });
 }
