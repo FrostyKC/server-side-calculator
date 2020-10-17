97d8f9953dd712222c48c318270ec21ea2880e29
Joe Thompson <smnindian30@yahoo.com>
2020-10-16 19:38:37 -0500

    created get route for results

--- server/server.js
+++ server/server.js
@@ -2,11 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
+const history = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(express.static('public'));
+
+// POST for receiving problems
+
+// calculate problem
+
+// GET results/history
+app.get('/results', (req, res) => {
+  res.send(history);
+});
 
 app.listen(PORT, () => {
   console.log('Server is running on port', PORT);
